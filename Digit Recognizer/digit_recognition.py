710f78efad4fbbb4b352165ca7509064bc807bac
myidispg <36075176+myidispg@users.noreply.github.com>
2018-09-06 20:20:48 +0530

    Added random forest

--- Digit Recognizer/digit_recognition.py
+++ Digit Recognizer/digit_recognition.py
@@ -46,11 +46,22 @@ classifier.add(Dense(10, init = 'uniform', activation = 'softmax'))
 
 classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 15)
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 30)
+
+# Fitting the naive bayes classifier to our dataset
+from sklearn.naive_bayes import GaussianNB
+classifier_naive_bayes = GaussianNB()
+classifier_naive_bayes.fit(X_train, y_train)
+
+# Training our Random Forest classifier
+from sklearn.ensemble import RandomForestClassifier
+classifier_forest = RandomForestClassifier(n_estimators=1000, criterion = 'entropy', random_state = 0)
+classifier_forest.fit(X_train, y_train)
 
 # Predicting the Test set results
-
 y_pred = classifier.predict(X_test)
+y_pred_bayes = classifier_naive_bayes.predict(X_test)
+y_pred_forest = classifier_forest.predict(X_test)
 
 # Convert decimals to 1 if greater than 0.5 else 0
 for i in range(0, 28000):
@@ -59,11 +70,20 @@ for i in range(0, 28000):
         
 # Decode one hot encoded variables
 y_pred = np.argmax(y_pred, axis=1)
+y_pred_forest = np.argmax(y_pred_forest, axis = 1)
 y_train = np.argmax(y_train, axis=1)
 
 # Creating a csv for predictions
 submission = pd.DataFrame(y_pred)
 filename = 'Digit Recognition.csv'
 
+submission_bayes = pd.DataFrame(y_pred_bayes)
+filename_bayes = 'Digit Recognition Naive Bayes.csv'
+
+submission_forest = pd.DataFrame(y_pred_forest)
+
 submission.to_csv(filename,index=False)
 
+submission_bayes.to_csv(filename_bayes,index=False)
+
+submission_forest.to_csv('Digit-Recognition-Forest.csv')
