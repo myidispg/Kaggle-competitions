2b7cd0b53f68988fa0945e78518f0def0128a9db
myidispg <36075176+myidispg@users.noreply.github.com>
2018-09-21 18:45:39 +0530

    Working on predictions

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -49,21 +49,88 @@ y_train = dataset_train.iloc[:, 3].values
 
 # Splitting the train dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X__train_test, y_train, y_train_test = train_test_split(X_train, y_train, test_size = 0.20, random_state = 0)
+X_train, X_train_test, y_train, y_train_test = train_test_split(X_train, y_train, test_size = 0.20, random_state = 0)
 
 # Fitting Logistic learning to the Training set
 from sklearn.linear_model import LogisticRegression
 classifier_logistics = LogisticRegression(random_state=0)
 classifier_logistics.fit(X_train, y_train)
 
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier_bayes = GaussianNB()
+classifier_bayes.fit(X_train, y_train)
+
 # Predicting the Test set results
-y_pred_logistics = classifier_logistics.predict(X__train_test)
+y_pred_logistics = classifier_logistics.predict(X_train_test)
+y_pred_bayes = classifier_bayes.predict(X_train_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm_logistics = confusion_matrix(y_train_test, y_pred_logistics)
+cm_bayes = confusion_matrix(y_train_test, y_pred_bayes)
+
+# Accuracy Score
+from sklearn.metrics import accuracy_score
+accuracy_logistics = accuracy_score(y_train_test, y_pred_logistics)
+accuracy_bayes = accuracy_score(y_train_test, y_pred_bayes)
 
 
 #-------------TEST DATASET---------------------------------------
 
 # Will commence work tomorrow. :P:P:P:P
+# Work on reducing taking only one phrase from each phase id.
+
+# Cleaning the dataset
+import re
+import nltk # library for nlp
+nltk.download('stopwords')
+# ^^  downloads a list of common words that are not relevant to reviews like this, that, prepositions etc
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+# ^^ Stemming, keeping only root of the words. Loved changes to love, hated changes to hate etc.
+corpus_test = []
+
+for i in range(0, len(dataset_train)):
+    
+    review = re.sub('[^a-zA-Z]', ' ', dataset_test['Phrase'][i] )
+    # ^^ Here we removed all the characters except a-z and A-Z and replaced them with a space.
+    review = review.lower() # Converted all characters to lowercase
+    review = review.split() # removed all spaces and returns a string
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = " ".join(review) # Convert list of processed words to a string seperated by space
+    corpus_test.append(review)
+    
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer 
+cv = CountVectorizer() # This keeps only the 1500 most occuring words.
+X_test = cv.fit_transform(corpus_test).toarray()
+
+# Splitting the train dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_train_test, y_train, y_train_test = train_test_split(X_train, y_train, test_size = 0.20, random_state = 0)
+
+# Fitting Logistic learning to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier_logistics = LogisticRegression(random_state=0)
+classifier_logistics.fit(X_train, y_train)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier_bayes = GaussianNB()
+classifier_bayes.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred_logistics = classifier_logistics.predict(X_train_test)
+y_pred_bayes = classifier_bayes.predict(X_train_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm_logistics = confusion_matrix(y_train_test, y_pred_logistics)
+cm_bayes = confusion_matrix(y_train_test, y_pred_bayes)
+
+# Accuracy Score
+from sklearn.metrics import accuracy_score
+accuracy_logistics = accuracy_score(y_train_test, y_pred_logistics)
+accuracy_bayes = accuracy_score(y_train_test, y_pred_bayes)
