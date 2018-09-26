ec4fca6982cd66ac94c84010252e0f0584737423
myidispg <36075176+myidispg@users.noreply.github.com>
2018-09-26 15:29:47 +0530

    Improved accuracy to 79.425 by adding ANN

--- Titanic Survival Predictor/titanic_survival_v3.py
+++ Titanic Survival Predictor/titanic_survival_v3.py
@@ -87,15 +87,7 @@ df['Fare'].fillna(fare_mean, inplace=True)
 
 del embarked_mode, fare_mean
 
-# Splitting the df into train and test set
-#df_train = df[df.Survived != 999]
-#df_test = df[df.Survived == 999]
-
-# del df
 #-----Finished data preprocessing--------
-
-df_train.isna().sum()
-df_test.isna().sum()
 
 # LabelEncoding and OneHotEncoding
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -105,3 +97,90 @@ df.Sex = labelencoder.fit_transform(df.Sex.values)
     # Encoding a dataframe
 df = pd.get_dummies(df,columns=['Embarked', 'Sex'], drop_first=True)
 #------Completed all encodings.------------
+
+# Splitting the df into train and test set
+df_train = df[df.Survived != 999]
+df_test = df[df.Survived == 999]
+
+
+df_test.drop('Survived', axis=1, inplace=True)
+
+del df
+
+df_train.isna().sum()
+df_test.isna().sum()
+
+# Converting to array
+X_train = df_train.iloc[:,[0,1,2,3,4,6,7,8]].values
+y_train = df_train.iloc[:, 5].values
+
+X_test = df_test.iloc[:, :].values
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Random Forest
+from sklearn.ensemble import RandomForestClassifier
+classifier_forest = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier_forest.fit(X_train, y_train)
+
+# Fitting Extra Tress Classifier
+from sklearn.ensemble import ExtraTreesClassifier
+etc=ExtraTreesClassifier(n_estimators=400)
+etc.fit(X_train,y_train)
+y_pred_extra = etc.predict(X_test)
+
+# Fitting a neural network
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+classifier.add(Dense(16, activation='relu', kernel_initializer='uniform', input_shape=(8,)))
+classifier.add(Dense(output_dim = 6, init='uniform', activation='relu'))
+classifier.add(Dense(output_dim = 1, init='uniform', activation='sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+classifier.summary()
+
+classifier.fit(X_train, y_train, batch_size=10, epochs = 100)
+
+# Predicting the Test set results
+y_pred_forest = classifier_forest.predict(X_test)
+
+y_pred = classifier.predict(X_test)
+# Reshape to a 1-D Array
+y_pred = np.reshape(y_pred, -1)
+
+# Convert to 0s and 1s.
+y_pred = [int(round(elem,0)) for elem in y_pred]
+
+# Making the Confusion Matrix for random forest and NN.
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_pred_extra, y_pred)
+
+index_list = []
+for i in range(892, 1310):
+    index_list.append(i)
+
+df_pred_extra = pd.DataFrame({'PassengerID': index_list, 'Survived':y_pred_extra}) 
+df_pred_forest = pd.DataFrame({'PassengerID': index_list, 'Survived':y_pred_forest}) 
+df_pred_nn = pd.DataFrame({'PassengerID': index_list, 'Survived':y_pred})
+
+del index_list, X_test, X_train, df_test, df_train, i, y_pred, y_train, y_pred_extra, y_pred_forest
+# Creating a csv for predictions of test_set
+filename = 'Titanic-Survival-Neural-Network.csv'
+df_pred_nn.to_csv(filename, index=False)
+
+# Creating a csv for predictions of test_set
+filename = 'Titanic-Survival-Extra_Trees.csv'
+df_pred_extra.to_csv(filename, index=False)
+
+# Creating a csv for predictions of test_set
+filename = 'Titanic-Survival-Extra_Trees.csv'
+df_pred_forest.to_csv(filename, index=False)
+
+
